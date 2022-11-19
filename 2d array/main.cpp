f91c6ea427499d1bd169c8527eb7481ccf20d8f3
vishal roy <vroy02243@gmail.com>
2022-11-19 22:14:16 +0530

    Initial Commit

---
+++ 2d array/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2d array
+//
+//  Created by Vishal Roy on 19/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
